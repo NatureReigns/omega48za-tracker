d7467a16575c222fc7170bed8dd10078a45b3333
NatureReigns <dave@naturereigns.com>
2025-12-13 10:33:31 +0200

    Create supabaseClient.jsx

---
+++ src/supabaseClient.jsx
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://ksjikdyauyyltbueymmg.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzamlrZHlhdXl5bHRidWV5bW1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MzU2NDcsImV4cCI6MjA4MTExMTY0N30.qwZwwSKXRP7Lrs6vjAd_UtsWKO-KXxNIW4T_QPK1jnE';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
