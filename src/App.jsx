d792af43aec3bf2ec3cb040e6749862464b47a47
NatureReigns <dave@naturereigns.com>
2025-12-13 10:34:49 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { supabase } from './main.jsx';
+import { supabase } from './supabaseClient.jsx';
 
 function App() {
   const [user, setUser] = useState(null);
